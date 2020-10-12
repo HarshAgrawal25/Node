1f8ec35c17dffc7d373d714429b52784198e7ae9
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-10-12 14:06:48 +0530

    handling HTTP get request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id:1,name:"course1"},
+    {id:2,name:"course2"},
+    {id:2,name:"course2"}
+]
+
 app.get('/',(req,res) => {
     res.send("Hello World   !!");
 });
 
 
 app.get('/api/courses',(req,res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res) => {
-    res.send(req.params.id);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if(!course) res.status(404).send("The course with given id not found")  ;
+  res.send(course);
 })
 
 app.get('/api/posts/:year/:month',(req,res) => {
@@ -21,6 +29,8 @@ app.get('/api/posts/:year/:month',(req,res) => {
 app.get('/api/posts/:year/:month',(req,res) => {
     res.send(req.query); 
 })
+
+
 // PORT
 const port = process.env.PORT || 3000
 app.listen(port, console.log(`Listening on port ${port}....`));
