b06e8c5f23811cca525816809108c570256b4cdf
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-10-12 20:54:10 +0530

    postman and input validation

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express');
 const app = express();
 
@@ -10,7 +11,7 @@ const courses = [
 ]
 
 app.get('/',(req,res) => {
-    res.send("Hello World   !!");
+    res.send("Hello World  Harsh !!");
 });
 
 
@@ -32,10 +33,20 @@ app.get('/api/posts/:year/:month',(req,res) => {
     res.send(req.query); 
 })
 
-
-
+//validation
 // HTTP post request
 app.post('/api/courses',(req,res) => {
+    const scheme = {
+      name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body,scheme)
+    
+    
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
     const course = {
         id : courses.length + 1,
         name: req.body.name
