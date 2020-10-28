ba341108123c1864efca56851bc4599be93ee572
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-10-28 11:46:50 +0530

    read write csv

--- advance/index.js
+++ advance/index.js
@@ -3,3 +3,6 @@ const rq= require("fs");
 //reading csv(comma seperated values) file by node js
 const text = rq.readFileSync('C:/Users/HP/Desktop/Ar Python/book.csv','utf-8');
 console.log(text);
+
+rq.writeFileSync('C:/Users/HP/Desktop/Ar Python/book100.csv',text);
+console.log("file written");
