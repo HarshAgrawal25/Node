f33afdce369d0906ea3e7ecf6b805f53ff2c47b8
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-11-01 20:00:48 +0530

    read and write async

--- advance/index.js
+++ advance/index.js
@@ -1,8 +1,28 @@
 const rq= require("fs");
 
-//reading csv(comma seperated values) file by node js
-const text = rq.readFileSync('C:/Users/HP/Desktop/Ar Python/book.csv','utf-8');
-console.log(text);
+//Blocking Synchrous Way
 
-rq.writeFileSync('C:/Users/HP/Desktop/Ar Python/book100.csv',text);
-console.log("file written");
+
+//reading csv(comma seperated values) file by node js
+// const text = rq.readFileSync('C:/Users/HP/Desktop/Ar Python/book.csv','utf-8');
+// console.log(text);
+
+// rq.writeFileSync('C:/Users/HP/Desktop/Ar Python/book100.csv',text);
+// console.log("file written");
+
+//Non-blocking Asynchronus way
+rq.readFile('E:/Documents(Laptop)/Node/advance/start.txt','utf-8',(err,data1)=>{
+    if(err) return console.log('ERROR')
+    rq.readFile(`E:/Documents(Laptop)/Node/advance/${data1}.txt`,'utf-8',(err,data2)=>{
+        console.log(data2)
+        rq.readFile(`E:/Documents(Laptop)/Node/advance/append.txt`,'utf-8',(err,data3)=>{
+            console.log(data3)
+            rq.writeFile('E:/Documents(Laptop)/Node/advance/final.txt',`${data2}\n${data3} 😁`,'utf-8',err => {
+                console.log('Your file has been written😄😄 ');
+            });
+        });
+    });
+});
+
+console.log("will read file!");
+
