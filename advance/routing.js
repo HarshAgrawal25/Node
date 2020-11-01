bc43a81fa2b939aba2a230e2c27989f13ab69255
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-11-01 22:48:54 +0530

    building simple API

--- advance/routing.js
+++ advance/routing.js
@@ -3,6 +3,15 @@
 
 const http = require("http");
 const url = require('url');
+const fs =require('fs')
+
+// fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8',(err,data) => {
+//     const productData = JSON.parse(data);
+// })
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req,res) => {
 
@@ -11,6 +20,11 @@ const server = http.createServer((req,res) => {
         res.end("This is OVERVIEW");
     }else if(pathname === '/product'){
         res.end('This is product')
+    }else if(pathname === '/api'){
+       
+            res.writeHead(200,{'Content-type':'application/json'})
+            res.end(data);
+        
     }else{
         res.writeHead(404,{
             'Content-type':'text/html',
